39ed1b5818d2137b04e3fb13c122f25fa4c779fa
NaveenKumarGupta73690 <114283748+NaveenKumarGupta73690@users.noreply.github.com>
2023-08-23 11:16:43 +0530

    Add files via upload

---
+++ AreaofCube.java
@@ -0,0 +1,13 @@
+//Area of cube
+import java.util.Scanner;
+class AreaofCube
+{
+	public static void main(String[] args)
+	{
+		Scanner obj=new Scanner(System.in);
+		float a;
+		System.out.println("Enter Any Number:");
+		a=obj.nextFloat();
+		System.out.println("Area of Cube:"+(6*(a*a)));
+	}
+}
